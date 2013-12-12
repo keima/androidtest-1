7194f620120e612e5e6f2dec8d788a4599bd5db6
Kouta Imanaka <k.imanaka@moggy.in>
2013-12-12 15:13:09 +0900

    Integrate DeployGate SDKs.

--- App/src/main/java/net/pside/android/sampleapp/SampleAppApplication.java
+++ App/src/main/java/net/pside/android/sampleapp/SampleAppApplication.java
@@ -2,6 +2,8 @@ package net.pside.android.sampleapp;
 
 import android.app.Application;
 
+import com.deploygate.sdk.DeployGate;
+
 /**
  * Created by keima on 2013/12/12.
  * Copyright (c) moggy,Inc. All rights Reserved.
@@ -11,5 +13,6 @@ public class SampleAppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        DeployGate.install(this);
     }
 }
