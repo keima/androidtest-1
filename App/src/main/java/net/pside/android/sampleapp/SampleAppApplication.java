6cfd2a4e0ca0d1cee3f5cea922472614b25c76d6
Kouta Imanaka <k.imanaka@moggy.in>
2013-12-12 17:45:29 +0900

    Integrate TestFlight SDKs.

--- App/src/main/java/net/pside/android/sampleapp/SampleAppApplication.java
+++ App/src/main/java/net/pside/android/sampleapp/SampleAppApplication.java
@@ -2,6 +2,10 @@ package net.pside.android.sampleapp;
 
 import android.app.Application;
 
+import com.testflightapp.lib.TestFlight;
+import com.testflightapp.lib.TestFlightProperties;
+import com.testflightapp.lib.core.networking.TestFlightHost;
+
 /**
  * Created by keima on 2013/12/12.
  * Copyright (c) moggy,Inc. All rights Reserved.
@@ -11,5 +15,6 @@ public class SampleAppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        TestFlight.takeOff(this, YOUR_TESTFLIGHT_APP_TOKEN);
     }
 }
