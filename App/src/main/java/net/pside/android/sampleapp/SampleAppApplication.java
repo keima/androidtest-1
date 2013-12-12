afc3cf7ececb24d0d70fdb2336b9d3ac470a1267
Kouta Imanaka <k.imanaka@moggy.in>
2013-12-12 17:34:11 +0900

    Integrate Vessel SDKs.

--- App/src/main/java/net/pside/android/sampleapp/SampleAppApplication.java
+++ App/src/main/java/net/pside/android/sampleapp/SampleAppApplication.java
@@ -2,6 +2,8 @@ package net.pside.android.sampleapp;
 
 import android.app.Application;
 
+import com.vessel.VesselSDK;
+
 /**
  * Created by keima on 2013/12/12.
  * Copyright (c) moggy,Inc. All rights Reserved.
@@ -11,5 +13,6 @@ public class SampleAppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        VesselSDK.initialize(getApplicationContext(), "YOUR_VESSEL_SECRET_CODE");
     }
 }
