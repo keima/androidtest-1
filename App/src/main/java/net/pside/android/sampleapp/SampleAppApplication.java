a1255c80c0cdfd1387644f31650945c8387295f6
Kouta Imanaka <k.imanaka@moggy.in>
2013-12-12 15:12:08 +0900

    Create Application class

---
+++ App/src/main/java/net/pside/android/sampleapp/SampleAppApplication.java
@@ -0,0 +1,15 @@
+package net.pside.android.sampleapp;
+
+import android.app.Application;
+
+/**
+ * Created by keima on 2013/12/12.
+ * Copyright (c) moggy,Inc. All rights Reserved.
+ */
+public class SampleAppApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
